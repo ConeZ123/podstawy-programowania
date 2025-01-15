a5ddfadf8409dc33a556511191dbefcf18bb3e5b
Mateusz Niski <340452@DC=sntis,DC=local>
2025-01-15 14:16:08 +0100

    Update2

--- lab13/SensorStats.py
+++ lab13/SensorStats.py
@@ -3,4 +3,5 @@ from SensorDataset import SensorDataset
 class SensorStats:
     def __init__(self, data):
         self._data = data.get_dataset()
-        
+        self._url = data.get_url()
+        self._sensor_type = data.get_sensor_type()
